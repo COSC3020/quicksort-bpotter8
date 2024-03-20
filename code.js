c9ddbab5af84e0c1c37b1b407b4d9db0d9bcf042
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-03-19 21:43:47 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,42 @@
 function quicksort(array) {
+    let stack = [];
+    let length = array.length;
+    
+    stack.push(0);
+    stack.push(length - 1);
+
+    while (stack.length > 0) {
+        let back = stack.pop();
+        let front = stack.pop();
+        let pivot = partition(array, front, back);
+
+        if (front < pivot - 1) {
+            stack.push(front);
+            stack.push(pivot - 1);
+        }
+
+        if (pivot + 1 < back) {
+            stack.push(pivot + 1);
+            stack.push(back);
+        }
+    }
+
     return array;
+}
+
+function partition(array, front, back) {
+    let pivot = array[back];
+    let i = front - 1;
+
+    for (let j = front; j < back; j++) {
+        if (array[j] < pivot) {
+            i++;
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    [array[i + 1], array[back]] = [array[back], array[i + 1]];
+    let pivInd = i + 1;
+    
+    return pivInd;
 }
